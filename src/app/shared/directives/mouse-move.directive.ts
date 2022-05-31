9d6c489786fbbd6432d08391efdcaa23ce5f4b4f
Andre <andrepalhares1989@gmail.com>
2022-05-31 21:52:23 +0100

    changing mouse active to mouse move directive

---
+++ src/app/shared/directives/mouse-move.directive.ts
@@ -0,0 +1,33 @@
+import { Directive, HostListener, Input } from '@angular/core';
+import { TimeOutService } from 'src/app/services/time-out.service';
+
+@Directive({
+  selector: '[mouseMove]',
+})
+export class MouseMoveDirective {
+  @Input('isActive') isActive: boolean = true;
+
+  mouseMoveClock;
+
+  constructor(private timeOutService: TimeOutService) {}
+
+  @HostListener('mousemove')
+  mouseenter() {
+    this.isActive
+      ? this.onMouseMoveRoutine()
+      : this.checkMouseMoveAndStopClock();
+  }
+
+  onMouseMoveRoutine() {
+    this.checkMouseMoveAndStopClock();
+
+    this.mouseMoveClock = setTimeout(() => {
+      this.timeOutService.setTimeOutClock();
+    }, 100);
+  }
+
+  checkMouseMoveAndStopClock() {
+    this.timeOutService.clearTimeoutClock();
+    this.mouseMoveClock && clearTimeout(this.mouseMoveClock);
+  }
+}
---
+++ src/app/shared/directives/mouse-move.spec.ts
@@ -0,0 +1,8 @@
+import { MouseMoveDirective } from 'src/app/shared/directives/mouse-move.directive';
+
+describe('MouseActiveDirective', () => {
+  it('should create an instance', () => {
+    const directive = new MouseMoveDirective();
+    expect(directive).toBeTruthy();
+  });
+});
